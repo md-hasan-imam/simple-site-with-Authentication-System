a63a15658d3ea0d16cfe0334e93b6c1c819e2c12
unknown <mohasanimam@gmail.com>
2022-04-10 11:10:24 +0600

    firebase authentication site starts

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxGlZWVN37w_CQrUwqJtApYMXyIs_62cA",
+  authDomain: "router-firebase-integrat-778a4.firebaseapp.com",
+  projectId: "router-firebase-integrat-778a4",
+  storageBucket: "router-firebase-integrat-778a4.appspot.com",
+  messagingSenderId: "361842861024",
+  appId: "1:361842861024:web:9b7a540251d7597b338185"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
